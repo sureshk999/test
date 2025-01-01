dedbb1bf90476a1537fa4aef884f509f43de3512
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2025-01-01 10:49:19 +1000

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -12,35 +12,26 @@ export default {
           ],
         },
         {
-          // Match all images
-          source: '/uploads/:path*',
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'public, max-age=31536000, immutable', // Cache uploaded images
-            },
-          ],
-        },
-        {
-          // Match all dynamic content (e.g., API routes)
-          source: '/api/:path*',
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'no-store', // Prevent caching of API responses
-            },
-          ],
-        },
-        {
-          // Match the home page or other dynamic pages
-          source: '/',
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'public, s-maxage=6000, stale-while-revalidate=600', // Cache for 6000 seconds at the edge
-            },
-          ],
-        },
+            // Match all images
+            source: '/uploads/:path*',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'public, max-age=31536000, immutable', // Cache uploaded images
+              },
+            ],
+          }, {
+            // Match all images
+            source: '/',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'public, max-age=31536000, immutable', // Cache uploaded images
+              },
+            ],
+          },
+        
+        
       ];
     },
   };
