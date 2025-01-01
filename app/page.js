f393f8923224722e84326f0a54ab0f2f9db31370
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2025-01-01 10:28:08 +1000

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My Next.js App</h1>
+      <Image
+        src="/default.jpg"
+        alt="Default Image"
+        width={400}
+        height={300}
+        priority
+      />
+    </div>
+  );
+}
